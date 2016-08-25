f853f0bb34c65d8310e818a52ac25be5b59377ee
Tatiana Korosteleva <t.korosteleva@rambler-co.ru>
2016-08-25 18:22:06 +0300

    #! add form;

--- javascript/custom.js
+++ javascript/custom.js
@@ -1,3 +1,48 @@
 (function () {
-  var wizard = $("#wizard").steps();
+  var form = $("#example-form");
+
+  form.validate({
+
+    errorPlacement: function errorPlacement(error, element) {
+      return true;
+    },
+
+    rules: {
+      phone: {
+        required: true
+      },
+      name: {
+        required: true
+      }
+    }
+  });
+
+  form.children("div").steps({
+    headerTag: "h3",
+    bodyTag: "section",
+    transitionEffect: "slideLeft",
+    labels: {
+      next: "Продолжить",
+      loading: "Загрузка ..."
+    },
+
+    onStepChanging: function (event, currentIndex, newIndex) {
+
+      if (currentIndex === 4 && newIndex === 5) {
+        if (form.valid()) {
+          console.log(form.serializeArray());
+          return true;
+        }
+      }
+
+      return form.valid();
+    }
+  });
+
+
+  $('.header__apply').on('click', function (evt) {
+    evt.preventDefault();
+    $('html, body').animate({ scrollTop: $(document).height() }, 'slow');
+  });
+
 }());
